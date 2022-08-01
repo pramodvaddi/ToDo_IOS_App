1d224cefa9db7518308a7f697bb78b4a32f6cf63
Pramod Vaddiraju <pramodvaddiraju@gmail.com>
2022-08-01 23:51:09 +0530

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Pramod Vaddiraju on 01/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
